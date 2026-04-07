a608a6350b5c0f5a521b0ecc2f74a96cd53de22c
mirkobechini <mirkobechini@gmail.com>
2026-04-07 21:50:16 +0200

    Add ProjectCard component and project data; update styles for layout and projects page

---
+++ src/data/projectData.js
@@ -0,0 +1,34 @@
+const spriteData = [
+    {
+        id: 1,
+        title: 'Project 1',
+        description: 'Description for Project 1',
+        preview: 'https://via.placeholder.com/300x200',
+        customer: 'Customer 1',
+        technologies: ['React', 'Node.js', 'MongoDB'],
+        repo: '#',
+        demo: "#"
+    },
+    {
+        id: 2,
+        title: 'Project 2',
+        description: 'Description for Project 2',
+        preview: 'https://via.placeholder.com/300x200',
+        customer: 'Customer 2',
+        technologies: ['Vue', 'Firebase'], //tecnologie categorizzate in frontend e backend ed hanno codice colore
+        repo: '#',
+        demo: "#"
+    },
+    {
+        id: 3,
+        title: 'Project 3',
+        description: 'Description for Project 3',
+        preview: 'https://via.placeholder.com/300x200',
+        customer: 'Customer 3',
+        technologies: ['Angular', 'Express', 'MySQL'],
+        repo: '#',
+        demo: "#"
+    }
+];
+
+export default spriteData;
