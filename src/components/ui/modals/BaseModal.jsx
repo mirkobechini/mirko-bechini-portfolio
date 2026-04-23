b0094a3ed8893eaae190f3139188e2977299e450
mirkobechini <mirkobechini@gmail.com>
2026-04-23 15:51:51 +0200

    Refactor BaseModal: rearrange JSX structure for improved accessibility and add flex-direction to modal overlay

--- src/components/ui/modals/BaseModal.jsx
+++ src/components/ui/modals/BaseModal.jsx
@@ -17,16 +17,10 @@ export default function BaseModal({ variant, closeModal }) {
 
     return (
         <div className={styles.modalOverlay} onClick={closeModal}>
-            <div
-                role="dialog"
-                aria-modal="true"
-                aria-labelledby="modal-title"
-                className={`${styles.gameModal} ${styles[`theme-${variant.theme}`]}`}
-                onClick={(e) => e.stopPropagation()}
-            >
-                <div className={styles.modalHeader}>
-                    <h2 id="modal-title">{variant.title}</h2>
-                </div>
+            <div className={styles.modalHeader}>
+                <h2 id="modal-title">{variant.title}</h2>
+            </div>
+            <div role="dialog" aria-modal="true" aria-labelledby="modal-title" className={`${styles.gameModal} ${styles[`theme-${variant.theme}`]}`} onClick={(e) => e.stopPropagation()}>
                 <div className={styles.modalContent}>
                     {variant.content}
                 </div>
