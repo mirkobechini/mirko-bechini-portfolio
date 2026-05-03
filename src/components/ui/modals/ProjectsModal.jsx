d43a97ef346886324edd6cd1353fd522fc95e3cf
mirkobechini <mirkobechini@gmail.com>
2026-05-03 09:38:25 +0200

    Enhance ProjectsModal: update filter UI with new styles and layout; add new button and wrapper images

--- src/components/ui/modals/ProjectsModal.jsx
+++ src/components/ui/modals/ProjectsModal.jsx
@@ -2,7 +2,6 @@ import styles from '../modalsCss/ProjectsModal.module.css';
 import { useMemo, useState } from 'react';
 import projectData from '../../../data/projectData';
 import resetButtonIcon from '../../../../public/assets/images/projectModal/reset_button.png';
-import filterContainer from '../../../../public/assets/images/projectModal/filterFieldset.png';
 
 export default function ProjectsModal() {
 
@@ -36,21 +35,21 @@ export default function ProjectsModal() {
     return (
         <div className={styles.deskContainer}>
             <div className={styles.filterContainer}>
-                <fieldset className={styles.filterFieldset}>
-                    <legend className={styles.filterLabel}>Filtra per tipologia:</legend>
+                <fieldset className={styles.filterWrapper}>
+                    <span className={styles.filterLabel}>Filtra per tipologia:</span>
                     <input type='radio' className={styles.visuallyHidden} id='frontend' name='filterType' value='frontend' checked={filterType === 'frontend'} onChange={e => setFilterType(e.target.value)} />
-                    <label htmlFor='frontend' className={filterType === 'frontend' ? styles.selectedFilter : ''}>Frontend</label>
+                    <label htmlFor='frontend' className={styles.buttonLabel + ' ' + (filterType === 'frontend' ? styles.selectedFilter : '')}>Frontend</label>
                     <input type='radio' className={styles.visuallyHidden} id='backend' name='filterType' value='backend' checked={filterType === 'backend'} onChange={e => setFilterType(e.target.value)} />
-                    <label htmlFor='backend' className={filterType === 'backend' ? styles.selectedFilter : ''}> Backend</label>
+                    <label htmlFor='backend' className={styles.buttonLabel + ' ' + (filterType === 'backend' ? styles.selectedFilter : '')}> Backend</label>
                     <input type='radio' className={styles.visuallyHidden} id='fullstack' name='filterType' value='fullstack' checked={filterType === 'fullstack'} onChange={e => setFilterType(e.target.value)} />
-                    <label htmlFor='fullstack' className={filterType === 'fullstack' ? styles.selectedFilter : ''}>Fullstack</label>
+                    <label htmlFor='fullstack' className={styles.buttonLabel + ' ' + (filterType === 'fullstack' ? styles.selectedFilter : '')}>Fullstack</label>
                 </fieldset>
-                <fieldset className={styles.filterFieldset}>
-                    <legend className={styles.filterLabel}>Filtra per azienda:</legend>
+                <fieldset className={styles.filterWrapper}>
+                    <span className={styles.filterLabel}>Filtra per azienda:</span>
                     <input type='radio' className={styles.visuallyHidden} id='boolean' name='filterCompany' value='boolean' checked={filterCompany === 'boolean'} onChange={e => setFilterCompany(e.target.value)} />
-                    <label htmlFor='boolean' className={filterCompany === 'boolean' ? styles.selectedFilter : ''}>Boolean</label>
+                    <label htmlFor='boolean' className={styles.buttonLabel + ' ' + (filterCompany === 'boolean' ? styles.selectedFilter : '')}>Boolean</label>
                     <input type='radio' className={styles.visuallyHidden} id='personal' name='filterCompany' value='personal' checked={filterCompany === 'personal'} onChange={e => setFilterCompany(e.target.value)} />
-                    <label htmlFor='personal' className={filterCompany === 'personal' ? styles.selectedFilter : ''}>Personali</label>
+                    <label htmlFor='personal' className={styles.buttonLabel + ' ' + (filterCompany === 'personal' ? styles.selectedFilter : '')}>Personali</label>
                 </fieldset>
                 <button className={styles.resetButtonContainer} onClick={() => { setFilterType(null); setFilterCompany(null); }} type="button">
                     <img src={resetButtonIcon} alt="Reset Filters" />
