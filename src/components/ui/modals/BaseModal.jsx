c9f037a28fd0350b478d7e59aa0e7cc4128bf1e1
mirkobechini <mirkobechini@gmail.com>
2026-05-06 08:45:37 +0200

    Add Fullstack Icon to Project Modal
    - Introduced a new PNG image file for the Fullstack icon in the project modal.

--- src/components/ui/modals/BaseModal.jsx
+++ src/components/ui/modals/BaseModal.jsx
@@ -18,7 +18,7 @@ export default function BaseModal({ variant, closeModal }) {
     return (
         <div className={styles.modalOverlay} onClick={closeModal}>
             <div className={styles.modalHeader}>
-                <h2 id="modal-title">{variant.title}</h2>
+                <h2 id="modal-title" className={styles.modalTitle}>{variant.title}</h2>
             </div>
             <div role="dialog" aria-modal="true" aria-labelledby="modal-title" className={`${styles.gameModal} ${styles[`theme-${variant.theme}`]}`} onClick={(e) => e.stopPropagation()}>
                 <div className={styles.modalContent}>
--- src/components/ui/modals/ProjectsModal.jsx
+++ src/components/ui/modals/ProjectsModal.jsx
@@ -1,7 +1,10 @@
 import styles from '../modalsCss/ProjectsModal.module.css';
 import { useMemo, useState } from 'react';
 import projectData from '../../../data/projectData';
-
+const frontendIcon = '/assets/images/projectModal/frontend-icon.png';
+const backendIcon = '/assets/images/projectModal/backend-icon.png';
+const fullstackIcon = '/assets/images/projectModal/fullstack-icon.png';
+const folderIcon = '/assets/images/projectModal/folder-icon.png';
 const resetButtonIcon = '/assets/images/projectModal/reset_button.png';
 
 export default function ProjectsModal() {
@@ -39,18 +42,18 @@ export default function ProjectsModal() {
                 <fieldset className={styles.filterWrapper}>
                     <span className={styles.filterLabel}>Filtra per tipologia:</span>
                     <input type='radio' className={styles.visuallyHidden} id='frontend' name='filterType' value='frontend' checked={filterType === 'frontend'} onChange={e => setFilterType(e.target.value)} />
-                    <label htmlFor='frontend' className={styles.buttonLabel + ' ' + (filterType === 'frontend' ? styles.selectedFilter : '')}>Frontend</label>
+                    <label htmlFor='frontend' className={styles.buttonLabel + ' ' + (filterType === 'frontend' ? styles.selectedFilter : '')}> <img className={styles.icon} src={frontendIcon} alt="Frontend" /> Frontend</label>
                     <input type='radio' className={styles.visuallyHidden} id='backend' name='filterType' value='backend' checked={filterType === 'backend'} onChange={e => setFilterType(e.target.value)} />
-                    <label htmlFor='backend' className={styles.buttonLabel + ' ' + (filterType === 'backend' ? styles.selectedFilter : '')}> Backend</label>
+                    <label htmlFor='backend' className={styles.buttonLabel + ' ' + (filterType === 'backend' ? styles.selectedFilter : '')}> <img className={styles.icon} src={backendIcon} alt="Backend" /> Backend</label>
                     <input type='radio' className={styles.visuallyHidden} id='fullstack' name='filterType' value='fullstack' checked={filterType === 'fullstack'} onChange={e => setFilterType(e.target.value)} />
-                    <label htmlFor='fullstack' className={styles.buttonLabel + ' ' + (filterType === 'fullstack' ? styles.selectedFilter : '')}>Fullstack</label>
+                    <label htmlFor='fullstack' className={styles.buttonLabel + ' ' + (filterType === 'fullstack' ? styles.selectedFilter : '')}> <img className={styles.icon} src={fullstackIcon} alt="Fullstack" /> Fullstack</label>
                 </fieldset>
                 <fieldset className={styles.filterWrapper}>
                     <span className={styles.filterLabel}>Filtra per azienda:</span>
                     <input type='radio' className={styles.visuallyHidden} id='boolean' name='filterCompany' value='boolean' checked={filterCompany === 'boolean'} onChange={e => setFilterCompany(e.target.value)} />
-                    <label htmlFor='boolean' className={styles.buttonLabel + ' ' + (filterCompany === 'boolean' ? styles.selectedFilter : '')}>Boolean</label>
+                    <label htmlFor='boolean' className={styles.buttonLabel + ' ' + (filterCompany === 'boolean' ? styles.selectedFilter : '')}> <img className={styles.icon} src={folderIcon} alt="Boolean" /> Boolean</label>
                     <input type='radio' className={styles.visuallyHidden} id='personal' name='filterCompany' value='personal' checked={filterCompany === 'personal'} onChange={e => setFilterCompany(e.target.value)} />
-                    <label htmlFor='personal' className={styles.buttonLabel + ' ' + (filterCompany === 'personal' ? styles.selectedFilter : '')}>Personali</label>
+                    <label htmlFor='personal' className={styles.buttonLabel + ' ' + (filterCompany === 'personal' ? styles.selectedFilter : '')}> <img className={styles.icon} src={folderIcon} alt="Personali" /> Personali</label>
                 </fieldset>
                 <button className={styles.resetButtonContainer} onClick={() => { setFilterType(null); setFilterCompany(null); }} type="button">
                     <img src={resetButtonIcon} alt="Reset Filters" />
