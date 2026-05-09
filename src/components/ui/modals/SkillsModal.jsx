ce0fd04a1c76f3c5b69268832848487635b85cbb
mirkobechini <mirkobechini@gmail.com>
2026-05-09 09:50:26 +0200

    Refactor SkillsModal: replace static skills list with dynamic rendering from skillsData

--- src/components/ui/modals/SkillsModal.jsx
+++ src/components/ui/modals/SkillsModal.jsx
@@ -1,19 +1,18 @@
 import styles from '../modalsCss/SkillsModal.module.css';
+import skillsData from '../../../data/skillsData';
 
 export default function SkillsModal() {
     return (
         <div className={styles.book}>
             <section className={styles.page}>
                 <h3>Competenze</h3>
-                <ul>
-                    <li><strong>Frontend</strong>: HTML5, CSS3, JavaScript</li>
-                    <li><strong>Backend</strong>: Node.js, PHP</li>
-                    <li><strong>Libraries</strong>: React, Axios</li>
-                    <li><strong>Frameworks</strong>: React Router, Bootstrap, Laravel, Express</li>
-                    <li><strong>Database</strong>: MySQL</li>
-                    <li><strong>Version Control</strong>: Git, GitHub</li>
-                    <li><strong>Tools</strong>: Blade, Vite, Postman, Figma</li>
-                </ul>
+                <div className={styles.skillsList}>
+                    {skillsData.map(skill => (
+                        <div className={`${styles.skillContainer} nes-container is-rounded is-centered`} key={skill.id} style={{ color: skill.color }}>
+                            <i className={`nes-icon is-small`}>{skill.icon}</i> 
+                        </div>
+                    ))}
+                </div>
             </section>
             <section className={styles.page}>
                 <h3>Formazione</h3>
