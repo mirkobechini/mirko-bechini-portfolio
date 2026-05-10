c6cf1ff91348ec2b049326a67c1235b1aef08b9b
mirkobechini <mirkobechini@gmail.com>
2026-05-10 18:32:00 +0200

    Enhancement: Add skill selection functionality and improve modal styles

--- src/components/ui/modals/SkillsModal.jsx
+++ src/components/ui/modals/SkillsModal.jsx
@@ -1,9 +1,18 @@
 import styles from '../modalsCss/SkillsModal.module.css';
 import skillsData from '../../../data/skillsData';
 import educationData from '../../../data/educationData';
-import { memo } from 'react';
+import { memo, useState } from 'react';
+
+
 
 const SkillsModal = memo(function SkillsModal() {
+
+    const [currentSkill, setCurrentSkill] = useState(null);
+
+    const handleClick = (skill) => {
+        setCurrentSkill(skill);
+    };
+
     return (
         <div className={styles.book}>
             <section className={styles.page}>
@@ -11,28 +20,35 @@ const SkillsModal = memo(function SkillsModal() {
                 <div className={styles.skillsList}>
                     {skillsData.map(skill => (
                         <div className={`${styles.skillCard} nes-container is-rounded`} key={skill.id} style={{ color: skill.color }}>
-                            <img className={`nes-icon is-medium`} src={skill.icon} alt={skill.skill} />
+                            <img className={`nes-icon is-medium`} src={skill.icon} alt={skill.skill} onClick={() => handleClick(skill)}/>
                         </div>
                     ))}
                 </div>
             </section>
-            <section className={styles.page}>
-                <h3>Formazione</h3>
-                <div className={styles.educationList}>
-                    {educationData.map(education => (
-                        <section className={styles.educationCard} key={education.id}>
-                            <span className={styles.educationPeriod}>{education.period}</span>
-                            <h5 className={styles.educationTitle}>{education.course} - {education.organization}</h5>
-                            <p className={styles.educationDescription}>{education.description}</p>
-                            <div className={styles.educationSkills}>
-                                {education.skills.map((skill, index) => (
-                                    <span key={index} className={styles.educationSkill}>{skill}</span>
-                                ))}
-                            </div>
-                        </section>
-                    ))}
-                </div>
-            </section>
+            {currentSkill === null ? (
+                <section className={styles.page}>
+                    <h3>Formazione</h3>
+                    <div className={styles.educationList}>
+                        {educationData.map(education => (
+                            <section className={styles.educationCard} key={education.id}>
+                                <span className={styles.educationPeriod}>{education.period}</span>
+                                <h5 className={styles.educationTitle}>{education.course} - {education.organization}</h5>
+                                <p className={styles.educationDescription}>{education.description}</p>
+                                <div className={styles.educationSkills}>
+                                    {education.skills.map((skill, index) => (
+                                        <span key={index} className={styles.educationSkill}>{skill}</span>
+                                    ))}
+                                </div>
+                            </section>
+                        ))}
+                    </div>
+                </section>
+            ) : (
+                <section className={styles.page}>
+                    <h3>{currentSkill.skill}</h3>
+                    <p>{currentSkill.description}</p>
+                </section>
+            )}
         </div>
     );
 });
