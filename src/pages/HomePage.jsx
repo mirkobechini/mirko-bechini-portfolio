4503f984bc48f58313d6de734052868d75c3962a
mirkobechini <mirkobechini@gmail.com>
2026-05-18 18:15:44 +0200

    Refactor: add tooltip tags to sprite buttons for improved accessibility and user experience

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -61,11 +61,21 @@ export default function HomePage() {
 
                 <img className="den-image" src={denBackground} onLoad={centerBackground} alt="Monkey Den" draggable="false" />
 
-                <button className='sprite monkey' aria-label="Sezione Chi Sono" title='About me' onClick={() => openModal(MODAL_IDS.ABOUT_ME)}></button>
-                <button className='sprite library' aria-label="Sezione Formazione & Competenze" title='Formation & Skills' onClick={() => openModal(MODAL_IDS.SKILLS)}></button>
-                <button className='sprite desk' aria-label="Sezione Esperienze & Progetti" title='Experiences & Projects' onClick={() => openModal(MODAL_IDS.PROJECTS)}></button>
-                <button className='sprite parrot' aria-label="Sezione Contatti" title='Contacts' onClick={() => openModal(MODAL_IDS.CONTACTS)}></button>
-                <button className='sprite painting' aria-label="Sezione Certificazioni" title='Certifications' onClick={() => openModal(MODAL_IDS.CERTIFICATIONS)}></button>
+                <button className='sprite monkey' aria-label="Sezione Chi Sono" title='About me' onClick={() => openModal(MODAL_IDS.ABOUT_ME)}>
+                    <div className="nes-container is-rounded sprite-tag">Chi Sono</div>
+                </button>
+                <button className='sprite library' aria-label="Sezione Formazione & Competenze" title='Formation & Skills' onClick={() => openModal(MODAL_IDS.SKILLS)}>
+                    <div className="nes-container is-rounded sprite-tag">Formazione & Competenze</div>
+                </button>
+                <button className='sprite desk' aria-label="Sezione Esperienze & Progetti" title='Experiences & Projects' onClick={() => openModal(MODAL_IDS.PROJECTS)}>
+                    <div className="nes-container is-rounded sprite-tag">Esperienze & Progetti</div>
+                </button>
+                <button className='sprite parrot' aria-label="Sezione Contatti" title='Contacts' onClick={() => openModal(MODAL_IDS.CONTACTS)}>
+                    <div className="nes-container is-rounded sprite-tag">Contatti</div>
+                </button>
+                <button className='sprite painting' aria-label="Sezione Certificazioni" title='Certifications' onClick={() => openModal(MODAL_IDS.CERTIFICATIONS)}>
+                    <div className="nes-container is-rounded sprite-tag">Certificazioni</div>
+                </button>
             </div>
             {activeSection && (<BaseModal variant={activeSection} closeModal={closeModal} role="dialog" />)}
         </div >
