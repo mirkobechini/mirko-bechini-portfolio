d5f8315bac86f1558dbe2718e197c08f5fec857e
mirkobechini <mirkobechini@gmail.com>
2026-05-29 21:40:09 +0200

    Refactor: Ensure image optimizer excludes temporary unused files in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     ViteImageOptimizer({
+      exclude: /_tmp-unused\//,
       png: { quality: 70 },
       jpg: { quality: 70 },
       jpeg: { quality: 70 },
