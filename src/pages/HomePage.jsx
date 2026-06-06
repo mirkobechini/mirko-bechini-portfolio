596ae81ad8bb8f66e16366077e48f22c72aa8b9c
mirkobechini <mirkobechini@gmail.com>
2026-06-06 12:23:18 +0200

    Fix: Update sprite-tag styles, add NES container for home sprites and add on focus tags

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -75,19 +75,19 @@ export default function HomePage() {
                 <img className="den-image" src={denBackground} onLoad={centerBackground} alt="Monkey Den" draggable="false" loading="eager" fetchPriority="high" decoding="async" width="1568" height="454" />
 
                 <button className='sprite monkey' aria-label="Sezione Chi Sono" title='About me' onClick={() => openModal(MODAL_IDS.ABOUT_ME)} onMouseEnter={() => preloadModalSprite(MODAL_IDS.ABOUT_ME)} onFocus={() => preloadModalSprite(MODAL_IDS.ABOUT_ME)} onTouchStart={() => preloadModalSprite(MODAL_IDS.ABOUT_ME)}>
-                    <div className="sprite-tag">Chi Sono</div>
+                    <div className="nes-container is-rounded sprite-tag">Chi Sono</div>
                 </button>
                 <button className='sprite library' aria-label="Sezione Formazione & Competenze" title='Formation & Skills' onClick={() => openModal(MODAL_IDS.BOOKSHELF)} onMouseEnter={() => preloadModalSprite(MODAL_IDS.BOOKSHELF)} onFocus={() => preloadModalSprite(MODAL_IDS.BOOKSHELF)} onTouchStart={() => preloadModalSprite(MODAL_IDS.BOOKSHELF)}>
-                    <div className="sprite-tag">Formazione & Competenze</div>
+                    <div className="nes-container is-rounded sprite-tag">Formazione & Competenze</div>
                 </button>
                 <button className='sprite desk' aria-label="Sezione Esperienze & Progetti" title='Experiences & Projects' onClick={() => openModal(MODAL_IDS.PROJECTS)} onMouseEnter={() => preloadModalSprite(MODAL_IDS.PROJECTS)} onFocus={() => preloadModalSprite(MODAL_IDS.PROJECTS)} onTouchStart={() => preloadModalSprite(MODAL_IDS.PROJECTS)}>
-                    <div className="sprite-tag">Esperienze & Progetti</div>
+                    <div className="nes-container is-rounded sprite-tag">Esperienze & Progetti</div>
                 </button>
                 <button className='sprite parrot' aria-label="Sezione Contatti" title='Contacts' onClick={() => openModal(MODAL_IDS.CONTACTS)} onMouseEnter={() => preloadModalSprite(MODAL_IDS.CONTACTS)} onFocus={() => preloadModalSprite(MODAL_IDS.CONTACTS)} onTouchStart={() => preloadModalSprite(MODAL_IDS.CONTACTS)}>
-                    <div className="sprite-tag">Contatti</div>
+                    <div className="nes-container is-rounded sprite-tag">Contatti</div>
                 </button>
                 <button className='sprite painting' aria-label="Sezione Certificazioni" title='Certifications' onClick={() => openModal(MODAL_IDS.CERTIFICATIONS)} onMouseEnter={() => preloadModalSprite(MODAL_IDS.CERTIFICATIONS)} onFocus={() => preloadModalSprite(MODAL_IDS.CERTIFICATIONS)} onTouchStart={() => preloadModalSprite(MODAL_IDS.CERTIFICATIONS)}>
-                    <div className="sprite-tag">Certificazioni</div>
+                    <div className="nes-container is-rounded sprite-tag">Certificazioni</div>
                 </button>
             </div>
             {activeSection && (<BaseModal key={`${activeSection.id}-${activeSection.sprite ?? ''}`} variant={activeSection} closeModal={closeModal} role="dialog" />)}
