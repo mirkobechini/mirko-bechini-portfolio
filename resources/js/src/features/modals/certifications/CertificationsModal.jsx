8e7d3a04e462215a8039a5f6f91fcc31e2fa541f
mirkobechini <mirkobechini@gmail.com>
2026-06-22 15:26:10 +0200

    fix: update handleCertClick function to remove certId parameter for improved clarity

--- resources/js/src/features/modals/certifications/CertificationsModal.jsx
+++ resources/js/src/features/modals/certifications/CertificationsModal.jsx
@@ -5,8 +5,8 @@ import { isValidLink } from '../../../utils/links';
 
 const CertificationsModal = memo(function CertificationsModal({ openModal }) {
 
-    function handleCertClick(certId) {
-        // TODO: Navigate to the specific formation modal for this certification
+    function handleCertClick() {
+        // TODO: Navigate to the specific formation modal for this certification (use certId to target the right entry)
         openModal(2);
     }
 
@@ -17,7 +17,7 @@ const CertificationsModal = memo(function CertificationsModal({ openModal }) {
                 certificationData.map(cert => (
 
                     <div className={styles['certificate-item']} key={cert.id}>
-                        <div className={styles.frame} onClick={() => handleCertClick(cert.id)} role="button" tabIndex={0} onKeyDown={(e) => { if (e.key === 'Enter' || e.key === ' ') handleCertClick(cert.id); }}>
+                        <div className={styles.frame} onClick={handleCertClick} role="button" tabIndex={0} onKeyDown={(e) => { if (e.key === 'Enter' || e.key === ' ') handleCertClick(); }}>
                             <img src={cert.preview} alt={cert.title} loading="lazy" decoding="async" width={cert.previewWidth ?? 1320} height={cert.previewHeight ?? 840} />
                         </div>
                         <p>
