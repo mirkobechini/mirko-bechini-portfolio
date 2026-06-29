396b37513f0c25157e06b665c2f537910c38d86a
mirkobechini <mirkobechini@gmail.com>
2026-06-29 15:01:56 +0200

    feat(CertificationsModal): pass certId as preselectEducation to navigate to the matching formation detail

--- resources/js/src/features/modals/certifications/CertificationsModal.jsx
+++ resources/js/src/features/modals/certifications/CertificationsModal.jsx
@@ -5,9 +5,8 @@ import { isValidLink } from '../../../utils/links';
 
 const CertificationsModal = memo(function CertificationsModal({ openModal }) {
 
-    function handleCertClick() {
-        // TODO: Navigate to the specific formation modal for this certification (use certId to target the right entry)
-        openModal(2);
+    function handleCertClick(certId) {
+        openModal(2, { preselectEducation: certId });
     }
 
     return (
@@ -17,7 +16,7 @@ const CertificationsModal = memo(function CertificationsModal({ openModal }) {
                 certificationData.map(cert => (
 
                     <div className={styles['certificate-item']} key={cert.id}>
-                        <div className={styles.frame} onClick={handleCertClick} role="button" tabIndex={0} onKeyDown={(e) => { if (e.key === 'Enter' || e.key === ' ') handleCertClick(); }}>
+                        <div className={styles.frame} onClick={() => handleCertClick(cert.id)} role="button" tabIndex={0} onKeyDown={(e) => { if (e.key === 'Enter' || e.key === ' ') handleCertClick(cert.id); }}>
                             <img src={cert.preview} alt={cert.title} loading="lazy" decoding="async" width={cert.previewWidth ?? 1320} height={cert.previewHeight ?? 840} />
                         </div>
                         <p>
